710c64ea0131f992175fb2c21358fae78f1a12a1
Duc111095 <huynhduc111095@gmail.com>
2025-08-01 07:20:16 +0700

    add some classes

--- src/main/java/com/ducnh/highperformance/collections/IntHashSet.java
+++ src/main/java/com/ducnh/highperformance/collections/IntHashSet.java
@@ -3,9 +3,13 @@ package com.ducnh.highperformance.collections;
 import static com.ducnh.highperformance.BitUtil.findNextPositivePowerOfTwo;
 import static com.ducnh.highperformance.collections.CollectionUtil.validateLoadFactor;
 
+import java.lang.reflect.Array;
 import java.util.AbstractSet;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.function.IntPredicate;
 import java.util.function.Predicate;
 
@@ -311,4 +315,310 @@ public class IntHashSet extends AbstractSet<Integer>{
 		
 		return removed;
 	}
-}
+	
+	public boolean removeAll(final IntHashSet col1) {
+		boolean removed = false;
+		for (final int value : col1.values) {
+			if (MISSING_VALUE != value) {
+				removed |= remove(value);
+			}
+		} 
+		
+		if (col1.containsMissingValue) {
+			removed |= remove(MISSING_VALUE);
+		}
+		
+		return removed;
+	}
+	
+	public boolean retainAll(final Collection<?> col1) {
+		boolean removed = false;
+		final int[] values = this.values;
+		final int length = values.length;
+		int i = 0;
+		for (; i < length; i++) {
+			final int value = values[i];
+			if (MISSING_VALUE != value && !col1.contains(value)) {
+				values[i] = MISSING_VALUE;
+				sizeOfArrayValues--;
+				removed = true;
+			}
+		}
+		
+		if (removed && sizeOfArrayValues > 0) {
+			rehash(values.length);
+		}
+		
+		if (containsMissingValue && !col1.contains(MISSING_VALUE)) {
+			containsMissingValue = false;
+			removed = true;
+		}
+		
+		return removed;
+	}
+	
+	public boolean retainAll(final IntHashSet col1) {
+		boolean removed = false;
+		final int length = values.length;
+		int i = 0;
+		for (; i < length; i++) {
+			final int value = values[i];
+			if (MISSING_VALUE != value && !col1.contains(value)) {
+				values[i] = MISSING_VALUE;
+				sizeOfArrayValues--;
+				removed = true;
+			}
+		}
+		
+		if (removed && sizeOfArrayValues > 0) {
+			rehash(values.length);
+		} 
+		
+		if (containsMissingValue && !col1.containsMissingValue) {
+			containsMissingValue = false;
+			removed = true;
+		}
+		
+		return removed;
+	}
+	
+	public IntIterator iterator() {
+		IntIterator iterator = this.iterator;
+		if (iterator == null) {
+			iterator = new IntIterator();
+			if (shouldAvoidAllocation) {
+				this.iterator = iterator;
+			}
+		}
+		
+		return iterator.reset();
+	}
+	
+	public void forEacHInt(final IntConsumer action) {
+		if (sizeOfArrayValues > 0) {
+			final int[] values = this.values;
+			for (final int v : values) {
+				if (MISSING_VALUE != v) {
+					action.accept(v);
+				}
+			}
+		}
+		
+		if (containsMissingValue) {
+			action.accept(MISSING_VALUE);
+		}
+	}
+	
+	public void copy(final IntHashSet that) {
+		if (values.length != that.values.length) {
+			throw new IllegalArgumentException("cannot copy object: masks not equal");
+		}
+		
+		System.arraycopy(that.values, 0, values, 0, values.length);
+		this.sizeOfArrayValues = that.sizeOfArrayValues;
+		this.containsMissingValue = that.containsMissingValue;
+	}
+	
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append('{');
+		
+		final int[] values = this.values;
+		for (final int value : values) {
+			if (MISSING_VALUE != value) {
+				sb.append(value).append(", ");
+			}
+		}
+		
+		if (containsMissingValue) {
+			sb.append(MISSING_VALUE).append(", ");
+		}
+		
+		if (sb.length() > 1) {
+			sb.setLength(sb.length() - 2);
+		}
+		
+		sb.append('}');
+		
+		return sb.toString();
+	}
+	
+	public <T> T[] toArray(final T[] a) {
+		final Class<?> componentType = a.getClass().getComponentType();
+		if (!componentType.isAssignableFrom(Integer.class)) {
+			throw new ArrayStoreException("cannot store Integers in array of type " + componentType);
+		}
+		
+		final int size = size();
+		final T[] arrayCopy = a.length >= size ? a : (T[]) Array.newInstance(componentType, size);
+		copyValues(arrayCopy);
+		
+		return arrayCopy;
+	}
+	
+	public Object[] toArray() {
+		final Object[] arrayCopy = new Object[size()];
+		copyValues(arrayCopy);
+		
+		return arrayCopy;
+	}
+	
+	private void copyValues(final Object[] arrayCopy) {
+		int i = 0;
+		final int[] values = this.values;
+		for (final int value : values) {
+			if (MISSING_VALUE != value) {
+				arrayCopy[i++] = value;
+			}
+		}
+		
+		if (containsMissingValue) {
+			arrayCopy[sizeOfArrayValues] = MISSING_VALUE;
+		}
+	}
+	
+	public boolean equals(final Object other) {
+		if (other == this) {
+			return true;
+		}
+		
+		if (other instanceof IntHashSet) {
+			final IntHashSet otherSet = (IntHashSet) other;
+			
+			return otherSet.containsMissingValue == containsMissingValue &&
+					otherSet.sizeOfArrayValues == sizeOfArrayValues &&
+					containsAll(otherSet);
+		}
+		
+		if (!(other instanceof Set)) {
+			return false;
+		}
+		
+		final Set<?> c = (Set<?>) other;
+		if (c.size() != size()) {
+			return false;
+		}
+		
+		try {
+			return containsAll(c);
+		} catch (final ClassCastException | NullPointerException ignore) {
+			return false;
+		}
+	}
+	
+	public int hashCode() {
+		int hashCode = 0;
+		for (final int value : values) {
+			if (MISSING_VALUE != value) {
+				hashCode += Integer.hashCode(value);
+			}
+		}
+		
+		if (containsMissingValue) {
+			hashCode += Integer.hashCode(MISSING_VALUE);
+		}
+		
+		return hashCode;
+	}
+	
+	public final class IntIterator implements Iterator<Integer> {
+		private int remaining;
+		private int positionCounter;
+		private int stopCounter;
+		private boolean isPositionValid = false;
+	
+		public IntIterator() {
+			
+		}
+		
+		IntIterator reset() {
+			remaining = size();
+			final int[] values = IntHashSet.this.values;
+			final int length = values.length;
+			int i = length;
+			
+			if (MISSING_VALUE != values[length - 1]) {
+				for (i = 0; i < length; i++) {
+					if (MISSING_VALUE == values[i]) {
+						break;
+					}
+				}
+			}
+			
+			stopCounter = i;
+			positionCounter = i + length;
+			isPositionValid = false;
+			
+			return this;
+		}
+		
+		public boolean hasNext() {
+			return remaining > 0;
+		}
+		
+		public int remaining() {
+			return remaining;
+		}
+		
+		public Integer next() {
+			return nextValue();
+		}
+		
+		public int nextValue() {
+			if (remaining == 1 && containsMissingValue) {
+				remaining = 0;
+				isPositionValid = true;
+				
+				return MISSING_VALUE;
+			}
+			
+			findNext();
+			
+			final int[] values = IntHashSet.this.values;
+			
+			return values[position(values)];
+		}
+		
+		public void remove() {
+			if (isPositionValid) {
+				if (0 == remaining && containsMissingValue) {
+					containsMissingValue = false;
+				} else {
+					final int[] values = IntHashSet.this.values;
+					final int position = position(values);
+					values[position] = MISSING_VALUE;
+					--sizeOfArrayValues;
+					
+					compactChain(position);
+				}
+				
+				isPositionValid = false;
+			} else {
+				throw new IllegalStateException();
+			}
+		}
+		
+		private void findNext() {
+			final int[] values = IntHashSet.this.values;
+			final int mask = values.length - 1;
+			isPositionValid = true;
+			
+			for (int i = positionCounter - 1, stop = stopCounter;  i >= stop; i--) {
+				final int index = i & mask;
+				if (MISSING_VALUE != values[index]) {
+					positionCounter = i;
+					--remaining;
+					return;
+				}
+			}
+			
+			isPositionValid = false;
+			throw new NoSuchElementException();
+		}
+		
+		private int position(
+				final int[] values) {
+			return positionCounter & (values.length - 1);
+		}
+	}
+} 
